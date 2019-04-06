d0b026c805253c654a6d05205c370c2049c731a4
Xiao Tan <tvytlx@gmail.com>
2019-04-06 18:15:21 +0800

    bumpversion

--- rqs.py
+++ rqs.py
@@ -11,7 +11,7 @@ from simplekv.fs import FilesystemStore
 
 STORAGE_PATH = os.path.join(Path.home(), ".rqs_storage")
 
-__version__ = "0.1"
+__version__ = "0.1.1"
 store = FilesystemStore(STORAGE_PATH)
 
 
